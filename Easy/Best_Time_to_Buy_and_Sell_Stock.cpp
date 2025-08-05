81797f6580340cf8e1e767bc7d654c355d4bd876
Harshika-Bharti <harshikabharti007@gmail.com>
2025-08-05 11:30:08 +0530

    Restore TwoSum and add Best Time to Buy/Sell Stock [Easy]

---
+++ Easy/Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = prices[0], sell, profit = 0;
+        for(int i = 0; i < prices.size() ; i++)
+        {
+            if(buy > prices[i])
+            {
+                buy = prices[i];
+            }
+            else
+            {
+                profit = max(profit, prices[i] - buy);
+            }
+        }
+        return profit;
+    }
+};
