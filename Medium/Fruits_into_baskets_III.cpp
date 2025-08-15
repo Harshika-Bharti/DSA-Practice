57ff5585ba29c21cc2d19b95630c1c3b7fd0d951
Harshika-Bharti <harshikabharti007@gmail.com>
2025-08-15 19:45:40 +0530

    added comments

--- Medium/Fruits_into_baskets_III.cpp
+++ Medium/Fruits_into_baskets_III.cpp
@@ -1,26 +1,24 @@
+// THIS SOLUTION IS NOT OPTIMAL
 class Solution {
 public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
         int count = 0, j;
-        unordered_map<int,int> store;
+        unordered_map<int,int> store; 
         for (int i = 0; i < fruits.size(); i++)
         {
             for (j = 0; j < baskets.size(); j++)
             {
-                if(store.find(j) != store.end())
+                if(store.find(j) != store.end()) //if already used basket if found, then skip it & look for another
                     continue;
-                if((baskets[j] > fruits[i]) || (baskets[j] == fruits[i]))
+                if((baskets[j] > fruits[i]) || (baskets[j] == fruits[i])) //if basket has capacity to accomodate the fruits
                 {
-                    store[j] = j;
-                    break;//continue;
+                    store[j] = j; //add the basket in the hashmap so that no other fruit will be placed in same basket
+                    break;        // if fruit is placed in a basket then loo for another fruit if it has also a basket 
                 }
             }
-            if(j == baskets.size())
-                count++;
+            if(j == baskets.size()) // fruit doesn't find basket for itself
+                count++; //count the no. of fruits which don't get a basket
         }
-        if( count > 0)
-            return count;
-        else
-            return 0;
+        return count;
     }
 };
