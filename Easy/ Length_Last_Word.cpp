b94765caddbaa5ac90895469f77c58db83aed040
Harshika-Bharti <harshikabharti007@gmail.com>
2026-02-11 08:59:49 +0530

    Added solution for Length of Last Word problem

---
+++ Easy/ Length_Last_Word.cpp
@@ -0,0 +1,16 @@
+// Length of Last Word
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.size()-1;
+        int c = 0;
+        while((i >= 0)&&(s[i] == ' '))
+            i--;
+        while((i >= 0)&&(s[i] != ' '))
+        {
+            c++;
+            i--;
+        }
+        return c;
+    }
+};
